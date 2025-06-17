c8e4ae24e1a26162159e38a51830417fcb3d9460
barodvinay22 <vinayb1@hexaware.com>
2025-06-17 14:25:29 +0530

    Update Demo.java
    (cherry picked from commit 6a8c22cd464bfd6879adfa9c9866d791a5a00ac2)

---
+++ src/main/java/Demo.java
@@ -0,0 +1,9 @@
+
+    public static void main(String...args){
+        System.out.println("Hello World");
+        int a=Integer.parseInt(args[0]);
+        int b= Integer.parseInt(args[1]);
+        System.out.println("Result="+ (a+b));
+    }
+
+}
