6a8c22cd464bfd6879adfa9c9866d791a5a00ac2
barodvinay22 <vinayb1@hexaware.com>
2025-06-17 14:25:29 +0530

    Update Demo.java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,7 +1,10 @@
 public class Demo {
 
-    public static void main(String...a){
+    public static void main(String...args){
         System.out.println("Hello World");
+        int a=Integer.parseInt(args[0]);
+        int b= Integer.parseInt(args[1]);
+        System.out.println("Result="+ (a+b));
     }
 
 }
