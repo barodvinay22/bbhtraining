82200f83d9db12ee40039b3d09b5ea05c18fae1b
Vinay Barod <vbarod@mobiquityinc.com>
2025-06-17 14:27:33 +0530

    Using Scanner suming two values

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Demo {
 
     public static void main(String...a){
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(n+m);
     }
 
 }
