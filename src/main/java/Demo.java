f7c191b3088ca793a596a0fef6504378ab0773db
Vinay Barod <vbarod@mobiquityinc.com>
2025-06-18 13:45:51 +0530

    Update Demo Java file with message for summing the values

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -9,7 +9,8 @@ public class Demo {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        System.out.println(n+m);
+        System.out.println("Sum Of n and m is "+n+m);
+        System.out.println("Sum Of a and b is "+a+b);
     }
 
 }
