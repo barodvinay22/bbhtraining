3fb7f434df6d0e3069e0c37b879d1e22645f762d
barodvinay22 <vinayb1@hexaware.com>
2025-06-18 13:47:15 +0530

    Update Demo Java file removed code (#4)
    * Update Demo Java file removed code
    
    * Update Demo Java file with message for summing the values
    
    ---------
    
    Co-authored-by: Vinay Barod <vbarod@mobiquityinc.com>

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Demo {
 
     public static void main(String...args){
-        System.out.println("Hello World");
         int a=Integer.parseInt(args[0]);
         int b= Integer.parseInt(args[1]);
         System.out.println("Result="+ (a+b));
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        System.out.println(n+m);
+        System.out.println("Sum Of n and m is "+n+m);
+        System.out.println("Sum Of a and b is "+a+b);
     }
 
 }
