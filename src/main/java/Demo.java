77756fa6743d510201b446e6fc61ab675b5a3ff7
Vinay Barod <vbarod@mobiquityinc.com>
2025-06-18 13:43:17 +0530

    Update Demo Java file with message

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Demo {
 
     public static void main(String...args){
+        System.out.println("Testing Git Rebase");
         int a=Integer.parseInt(args[0]);
         int b= Integer.parseInt(args[1]);
         System.out.println("Result="+ (a+b));
