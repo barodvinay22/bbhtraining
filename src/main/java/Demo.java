74d9256db05ac7fefed0e3b25ebaf81461aa1965
Vinay Barod <vbarod@mobiquityinc.com>
2025-06-18 13:42:15 +0530

    Update Demo Java file removed code

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Demo {
 
     public static void main(String...args){
-        System.out.println("Hello World");
         int a=Integer.parseInt(args[0]);
         int b= Integer.parseInt(args[1]);
         System.out.println("Result="+ (a+b));
